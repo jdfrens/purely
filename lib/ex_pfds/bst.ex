10a88860484a3baa134d73f4188d06dc1d89e7cc
Jeremy D. Frens <jdfrens@gmail.com>
2016-04-10 15:12:47 -0500

    Added type specs for BST.
    One warning remains.

--- lib/ex_pfds/bst.ex
+++ lib/ex_pfds/bst.ex
@@ -3,17 +3,30 @@ defmodule ExPfds.BST do
   Implementation of a purely functional binary search tree.
   """
 
+  @typedoc """
+  An empty tuple for an empty tree; a three-tuple (left, value, right)
+  for a general node.
+  """
+  @type empty_bst :: {}
+  @type nonempty_bst :: {bst, any(), bst}
+  @type bst :: empty_bst | nonempty_bst
+
   @empty {}
 
+  @spec empty() :: empty_bst
   def empty, do: @empty
 
+  @spec node(any()) :: nonempty_bst
   def node(value), do: node(@empty, value, @empty)
+  @spec node(bst, any(), bst) :: nonempty_bst
   def node(left, value, right) do
     {left, value, right}
   end
 
-  def inorder(@empty), do: []
-  def inorder({left, value, right}) do
-    inorder(left) ++ [value] ++ inorder(right)
+  @spec inorder(bst()) :: list(any())
+  def inorder(bst), do: inorder(bst, []) |> Enum.reverse
+  defp inorder(@empty, acc), do: acc
+  defp inorder({left, value, right}, acc) do
+    inorder(right, [value | inorder(left, acc)])
   end
 end
