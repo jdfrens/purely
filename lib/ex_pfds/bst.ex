385136335fb26efdbfe67e1403dec8a6a37d0887
Jeremy D. Frens <jdfrens@gmail.com>
2016-04-10 16:09:19 -0500

    Store a key and a value in a BST node.

--- lib/ex_pfds/bst.ex
+++ lib/ex_pfds/bst.ex
@@ -7,8 +7,9 @@ defmodule ExPfds.BST do
   An empty tuple for an empty tree; a three-tuple (left, value, right)
   for a general node.
   """
+  @type key_value :: {any, any}
   @type empty_bst :: {}
-  @type nonempty_bst :: {bst, any(), bst}
+  @type nonempty_bst :: {bst, key_value, bst}
   @type bst :: empty_bst | nonempty_bst
 
   @empty {}
@@ -16,29 +17,25 @@ defmodule ExPfds.BST do
   @spec empty() :: empty_bst
   def empty, do: @empty
 
-  @spec build(any()) :: nonempty_bst
-  def build(value), do: build(@empty, value, @empty)
-  @spec build(bst, any(), bst) :: nonempty_bst
-  def build(left, value, right) do
-    {left, value, right}
-  end
+  @spec build(key_value) :: nonempty_bst
+  def build(kv), do: build(@empty, kv, @empty)
+  @spec build(bst, key_value, bst) :: nonempty_bst
+  def build(l, kv, r), do: {l, kv, r}
 
   @spec inorder(bst()) :: list(any())
   def inorder(bst), do: inorder(bst, []) |> Enum.reverse
   defp inorder(@empty, acc), do: acc
-  defp inorder({left, value, right}, acc) do
-    inorder(right, [value | inorder(left, acc)])
+  defp inorder({l, kv, r}, acc) do
+    inorder(r, [kv | inorder(l, acc)])
   end
 
-  @spec insert(bst(), any()) :: bst()
-  def insert(@empty, new_value) do
-    build(new_value)
+  @spec insert(bst(), key_value()) :: bst()
+  def insert(@empty, new_kv), do: build(new_kv)
+  def insert({l, {k,v}, r}, {nk,nv}) when nk < k do
+    build(insert(l, {nk,nv}), {k,v}, r)
   end
-  def insert({left, value, right}, new_value) when new_value < value do
-    build(insert(left, new_value), value, right)
+  def insert({l, {k,v}, r}, {nk,nv}) when k < nk do
+    build(l, {k,v}, insert(r, {nk,nv}))
   end
-  def insert({left, value, right}, new_value) when new_value > value do
-    build(left, value, insert(right, new_value))
-  end
-  def insert(bst, _), do: bst
+  def insert(bst, {_,_}), do: bst
 end
--- test/ex_pfds/bst_test.exs
+++ test/ex_pfds/bst_test.exs
@@ -36,23 +36,24 @@ defmodule ExPfds.BSTTest do
   test "insert several values into empty tree in order" do
     bst =
       BST.empty
-      |> BST.insert(23)
-      |> BST.insert(46)
-      |> BST.insert(92)
-    assert BST.inorder(bst) == [23, 46, 92]
+      |> BST.insert({23,23})
+      |> BST.insert({46,46})
+      |> BST.insert({92,92})
+    assert BST.inorder(bst) == [{23,23}, {46,46}, {92,92}]
   end
 
   test "insert several values into empty tree in reverse order" do
     bst =
       BST.empty
-      |> BST.insert(92)
-      |> BST.insert(46)
-      |> BST.insert(23)
-    assert BST.inorder(bst) == [23, 46, 92]
+      |> BST.insert({92,92})
+      |> BST.insert({46,46})
+      |> BST.insert({23,23})
+    assert BST.inorder(bst) == [{23,23}, {46,46}, {92,92}]
   end
 
   property :insert do
     for_all xs in list(int) do
+      xs = Enum.zip(xs, xs)
       bst = Enum.reduce(xs, BST.empty, fn x, acc -> BST.insert(acc, x) end)
       BST.inorder(bst) == Enum.uniq(Enum.sort(xs))
     end
