2513c10e26b192200ec0a7b65b8dacc9c0fa0bc9
Jeremy D. Frens <jdfrens@gmail.com>
2016-08-28 17:10:33 -0500

    Lots of BST computations
    Matches Map pretty closely.

--- lib/purely/bst.ex
+++ lib/purely/bst.ex
@@ -31,11 +31,21 @@ defmodule Purely.BST do
   end
 
   # TODO: new(enumerable, transform)
+  @spec new(Enum.t, (term -> {key, value})) :: bst
+  def new(enumerable, transform) do
+    enumerable
+    |> Enum.map(transform)
+    |> new
+  end
 
   defp build(kv), do: build(kv, @empty, @empty)
   defp build(kv, l, r), do: {kv, l, r}
 
   # TODO: equal?(bst1, bst2)
+  @spec equal?(bst, bst) :: boolean
+  def equal?(bst1, bst2) do
+    inorder(bst1) === inorder(bst2)
+  end
 
   @doc """
   Returns a list of the key-value pairs in the tree in order, sorted
@@ -53,61 +63,122 @@ defmodule Purely.BST do
   """
   @spec put(bst, key, value) :: bst
   def put(@empty, key, val), do: build({key, val})
-  def put({{k, v}, l, r}=bst, key, val) do
+  def put({{k, v}, l, r}, key, val) do
     cond do
       key < k ->
         build({k,v}, put(l, key, val), r)
       k < key ->
         build({k, v}, l, put(r, key, val))
       true ->
-        bst
+        build({k, val}, l, r)
     end
   end
-  # TODO: put_new(bst, key, val)
-  # TODO: put_new_lazy(bst, key, fun)
 
-  defp flipped_put({key, val}, bst), do: put(bst, key, val)
+  @spec put_new(bst, key, value) :: bst
+  def put_new(bst, key, val) do
+    cond do
+      has_key?(bst, key) ->
+        bst
+      true ->
+        put(bst, key, val)
+    end
+  end
 
   @doc """
   Returns the value mapped to the given key; if the key is not found,
   then `nil` is returned.
   """
-  @spec get(bst, key) :: value
-  def get(bst, key), do: get(bst, key, nil)
+  @spec put_new_lazy(bst, key, (() -> value)) :: bst
+  def put_new_lazy(bst, key, fun) do
+    cond do
+      has_key?(bst, key) ->
+        bst
+      true ->
+        put(bst, key, fun.())
+    end
+  end
+
+  defp flipped_put({key, val}, bst), do: put(bst, key, val)
 
   @doc """
   Returns the value mapped to the given key; if the key is not found,
   then the specified default value is returned.
   """
+  @spec get(bst, key) :: value
   @spec get(bst, key, value) :: value
-  def get(@empty, _, default), do: default
-  def get({{k, v}, l, r}, key, default) do
+  def get(bst, key, default \\ nil), do: get_bst(bst, key, default)
+
+  defp get_bst(@empty, _, default), do: default
+  defp get_bst({{k, v}, l, r}, key, default) do
     cond do
       key < k ->
-        get(l, key, default)
+        get_bst(l, key, default)
       k < key ->
-        get(r, key, default)
+        get_bst(r, key, default)
       true ->
         v
     end
   end
 
-  # TODO: get_and_update(bst, key, fun)
-  # TODO: get_and_update!(bst, key, fun)
+  @spec get_lazy(bst, key, (() -> value)) :: value
+  def get_lazy(bst, key, fun) do
+    cond do
+      has_key?(bst, key) ->
+        get(bst, key)
+      true ->
+        fun.()
+    end
+  end
 
-  # TODO: get_lazy(bst, key, fun)
 
-  # TODO: pop(bst, key, default \\ nil)
-  # TODO: pop_lazy(bst, key, fun)
+  """
+  @spec get_and_update(bst, key, (value -> {get, value} | :pop)) :: {get, bst} when get: term
+  def get_and_update(bst, key, fun) do
+    current = get(bst, key)
+    case fun.(current) do
+      {get, update} -> {get, put(bst, key, update)}
+      :pop          -> {current, delete(bst, key)}
+    end
+  end
 
-  # TODO: fetch(bst, key)
-  # TODO: fetch!(bst, key)
 
-  # TODO: update(map, key, initial, fun)
-  # TODO: update!(map, key, fun)
+  """
+  @spec get_and_update!(bst, key, (value -> {get, value})) :: {get, bst} | no_return when get: term
+  def get_and_update!(bst, key, fun) do
+    if has_key?(bst, key) do
+      current = get(bst, key)
+      case fun.(current) do
+        {get, update} -> {get, put(bst, key, update)}
+        :pop          -> {current, delete(bst, key)}
+      end
+    else
+      :erlang.error({:badkey, key})
+    end
+  end
 
   @doc """
   Returns true if the key is in the BST, false otherwise.
+  @spec update(bst, key, value, (value -> value)) :: bst
+  def update(bst, key, initial, fun) do
+    cond do
+      has_key?(bst, key) ->
+        value = get(bst, key)
+        put(bst, key, fun.(value))
+      true ->
+        put(bst, key, initial)
+    end
+  end
+
+  @spec update!(bst, key, (value -> value)) :: bst | no_return
+  def update!(bst, key, fun) do
+    cond do
+      has_key?(bst, key) ->
+        update(bst, key, :does_not_matter, fun)
+      true ->
+        :erlang.error({:badkey, key})
+    end
+  end
+
   """
   @spec has_key?(bst, key) :: boolean
   def has_key?(@empty, _), do: false
@@ -130,19 +201,85 @@ defmodule Purely.BST do
     inorder(bst) |> Enum.map(fn {k,_} -> k end)
   end
 
-  # TODO: values(bst)
 
-  # TODO: merge(bst1, bst2)
-  # TODO: merge(bst1, bst2, callback)
 
-  # TODO: split(bst, keys)
+  @spec values(bst) :: [value]
+  def values(bst) do
+    inorder(bst) |> Enum.map(fn {_,v} -> v end)
+  end
 
-  # TODO: take(bst, keys)
+  @spec merge(bst, bst) :: bst
+  def merge(@empty, bst2), do: bst2
+  def merge(bst1, @empty), do: bst1
+  def merge(bst1, {{k,v}, bst2l, bst2r}) do
+    bst1
+    |> put(k, v)
+    |> merge(bst2l)
+    |> merge(bst2r)
+  end
 
-  # TODO: to_list(bst)
+  @spec merge(bst, bst, (key, value, value -> value)) :: bst
+  def merge(@empty, bst2, _), do: bst2
+  def merge(bst1, @empty, _), do: bst1
+  def merge(bst1, {{k,v}, bst2l, bst2r}, fun) do
+    bst1
+    |> merge_key(k, v, fun)
+    |> merge(bst2l, fun)
+    |> merge(bst2r, fun)
+  end
+
+  defp merge_key(bst, k, v, fun) do
+    new_value =
+      if has_key?(bst, k), do: fun.(k, get(bst, k), v), else: v
+    put(bst, k, new_value)
+  end
 
   @doc """
   Deletes the given key (and its value) from the BST.
+  @spec split(bst, Enumerable.t) :: {bst, bst}
+  def split(bst, keys) do
+    Enum.reduce(keys, {new, bst}, fn key, {bst1, bst2} ->
+      if has_key?(bst2, key) do
+        {value, bst2} = pop(bst2, key)
+        {put(bst1, key, value), bst2}
+      else
+        {bst1, bst2}
+      end
+    end)
+  end
+
+  @spec take(bst, Enumerable.t) :: bst
+  def take(bst, keys) do
+    {taken_bst, _} = split(bst, keys)
+    taken_bst
+  end
+
+  @spec drop(bst, Enumerable.t) :: bst
+  def drop(bst, keys) do
+    {_, dropped_bst} = split(bst, keys)
+    dropped_bst
+  end
+
+  @spec to_list(bst) :: [{key, value}]
+  def to_list(bst) do
+    inorder(bst)
+  end
+
+  @spec pop(bst, key, value) :: {value, bst}
+  def pop(bst, key, default \\ nil) do
+    {get(bst, key, default), delete(bst, key)}
+  end
+
+  @spec pop_lazy(bst, key, (() -> value)) :: {value, bst}
+  def pop_lazy(bst, key, fun) do
+    cond do
+      has_key?(bst, key) ->
+        pop(bst, key)
+      true ->
+        pop(bst, key, fun.())
+    end
+  end
+
   """
   @spec delete(bst, key) :: bst
   def delete(@empty, _), do: @empty
@@ -156,8 +293,6 @@ defmodule Purely.BST do
         promote_leftmost(l, r)
     end
   end
-
-  # TODO: drop(bst, keys)
 
   @docp """
   Purely a structural traversal to remove and return the leftmost
